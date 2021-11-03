d8f887e89bf8b3373a4df5932b37015287646656
Ashwin Dara <48425033+Ashwn-Dar@users.noreply.github.com>
2021-11-03 00:27:41 -0700

    Added a temporary graphing function after getting the nth samples function to work. Need to now (1) write function to generate Perlin Noise (2) fix the plot functions so that the correct values of x correspond to the right y values.

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import random
+import matplotlib.pyplot as plt
 
 class Grid:
-    points = {}
+    def __init__(self):
+        self.points = {}
 
-    def __init__(self, size):
-        self.size = size
-        self.points = Grid.points
-
-    def interpolate_all(self, type):
+    def interpolate_all(self):
         pass
 
-    def configure(self):
+    def __configure_plot_labels(self):
         pass
+
+    def plot(self):
+        self.x_values = [int(x) for x in self.points.keys()]
+        self.x_values.sort()
+        # Correctly map it later tomorrow
+        self.y_values = [int(self.points[ self.x_values[y]] ) for y in range len(self.x_values)]
+        plt.plot(self.x_values, self.y_values)
+        plt.xlabel("X values")
+        plt.ylabel("Intensity")
+        plt.show()
+
+
+
+
 
 
 def generate_seed_array(max_value, size):
@@ -25,9 +37,20 @@ def generate_interpolation(type, input_list):
     pass
 
 
-def sample_nth(input_list, n):
-    for i in range(0, len(input_list), n):
-        pass
+def sample_nth(grid, seed_list, step_size, scale):
+    """
+    :param grid: this will be the grid instance that the function will add points to
+    :param seed_list: this is the randomly generated array that the function will pull from
+    :param scale: this will be a number from 0 to 1.0 that we will multiply by the number
+    that we pull from
+    :return: None
+
+    """
+    curr_index = 0
+    while curr_index < len(seed_list) - 1:
+        grid.points[curr_index] = grid.points.get(curr_index, 0) + scale * seed_list[curr_index]
+        curr_index += step_size
+    grid.points[len(seed_list) - 1] = grid.points[0]
 
 
 def main():
@@ -35,6 +58,5 @@ def main():
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    print("Compiled")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
