1d43eec27e7933ccd979aed4a2eda002fcefaefc
Ashwin Dara <48425033+Ashwn-Dar@users.noreply.github.com>
2021-11-04 23:28:16 -0700

    Created the draw function. Unfortunately, it just draws a single rectangle of a single shade, so it is not working as intended. With this, I will need to debug most likely the draw function, particularily something with the for loop within the draw_array function of this class.

--- main.py
+++ main.py
@@ -1,26 +1,65 @@
 import random
 import matplotlib.pyplot as plt
+from tkinter import Tk, Canvas, Frame, BOTH
+
+def from_rgb(rgb):
+    """translates an rgb tuple of int to a tkinter friendly color code
+    """
+    return "#%02x%02x%02x" % rgb
+
+class Draw1D(Frame):
+
+    def __init__(self, perlin_array):
+        super().__init__()
+        self.perlin_array = perlin_array
+        self.initUI()
+
+    def draw_array(self, canvas):
+        i = 1
+        # 255, 255, 255 is white
+        if self.perlin_array:
+            print(self.perlin_array)
+            for z in self.perlin_array:
+                canvas.create_rectangle(i*20, 10, 5, 200, outline="red",
+                                        fill=from_rgb((int(z*255), int(z*255), int(z*255))))
+                print(from_rgb((int(z*255), int(z*255), int(z*255))))
+                i += 1
+
+    def initUI(self):
+        self.master.title("1D Perlin Noise")
+        self.pack(fill=BOTH, expand=1)
+
+        canvas = Canvas(self)
+        self.draw_array(canvas)
+        canvas.pack(fill=BOTH, expand=1)
 
 
 class Grid:
     def __init__(self):
         self.points = {}
+        self.x_values = 0
+        self.y_values = 0
 
-    def interpolate_all(self):
-        pass
+    def interpolate(self, x):
+        ep_positive = 0
+        ep_negative = 0
 
-    def __configure_plot_labels(self):
-        pass
-
-    def plot(self):
+    def configure_perlin_array(self):
         self.x_values = [int(x) for x in self.points.keys()]
         self.x_values.sort()
+        self.y_values = [float(self.points[x]) for x in self.x_values]
+
+    def plot(self):
         # Correctly map it later tomorrow
-        self.y_values = [int(self.points[x]) for x in self.x_values]
+        self.configure_perlin_array()
         plt.plot(self.x_values, self.y_values)
         plt.xlabel("X values")
         plt.ylabel("Intensity")
         plt.show()
+
+    def generate_array(self):
+        self.configure_perlin_array()
+        return self.y_values
 
 
 def generate_seed_array(max_value, size):
@@ -49,16 +88,29 @@ def sample_nth(grid, seed_list, step_size, scale):
         curr_index += step_size
     grid.points[len(seed_list) - 1] = grid.points[0]
 
+def draw1d():
+    pass
+
+def recurse_sample(grid, seed_list, step_size, scale):
+    if step_size == 1:
+        return
+    sample_nth(grid, seed_list, step_size, scale)
+    recurse_sample(grid, seed_list, step_size//2, scale/2)
 
 def generate_perlin_noise(grid, size):
-    seed_array = generate_seed_array(100, size - size % 2)
-    return # Need to fill out this functionn
+    seed_array = generate_seed_array(1, size - (size % 2))
+    recurse_sample(grid, seed_array, len(seed_array), 1)
 
 
 def main():
-    pass
+    root = Tk()
+    grid1 = Grid()
+    generate_perlin_noise(grid1, 64)
+    ex = Draw1D(grid1.generate_array())
+    root.geometry("500x500")
+    root.mainloop()
 
 
 if __name__ == '__main__':
-    print("Compiled")
+    main()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
